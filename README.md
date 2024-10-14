5781a1b0d013f4ec99e2d4619a545803b10a2d58
Gitssalah <salahalaoui4ever@gmail.com>
2024-10-14 16:09:02 +0100

    small change

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # compiler-with-rust
+ <h3>Step 1 : Lexical analysis</h3>
+ <p>According to wikipedia: Lexical analysis is conversion of a text into (semantically or syntactically) meaningful lexical tokens belonging to categories defined by a "lexer" program </p>
+
+ ![img.png](img.png) 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-mod abstarctSyntaxTree;
+mod abstract_syntax_tree;
 
 fn main() {
-    println!("Hello, world!");
+    let vec = vec![1, 5, 13, 40];
+    let mut iter = vec.iter().peekable();
+
+    // Peeking at the first element without advancing
+    println!("Peek: {:?}", iter.peek()); // Outputs: Some(&1)
+
+    // Using next() to advance the iterator
+    println!("Next: {:?}", iter.next()); // Outputs: Some(1)
+
+    // Peeking at the second element now
+    println!("Peek: {:?}", iter.peek()); // Outputs: Some(&2)
+    println!("Next: {:?}", iter.next()); // Outputs: Some(2)
+
+    // Continue with the rest of the iteration
+    while let Some(val
+    ) = iter.next() {
+        println!("Value: {}", val);
+    }
 }
