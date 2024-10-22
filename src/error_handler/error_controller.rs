372853b247443ec6c092d8033ceb49ab8eeb6770
Gitssalah <salahalaoui4ever@gmail.com>
2024-10-22 22:24:44 +0100

    step 3

--- src/error_handler/error_controller.rs
+++ src/error_handler/error_controller.rs
@@ -1,4 +1,7 @@
 pub enum ErrorType {
     SyntaxError(String),
     ValueError(String),
+    TypeError(String)
 }
+
+// todo add a handler that will receive error from the enum of token and determine the error type
